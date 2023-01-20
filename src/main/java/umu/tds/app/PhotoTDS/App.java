8efa56895abb5323cb60ed004b12df1e5567bef4
Juan Carlos Segura García <seguragarcia.jc@gmail.com>
2023-01-20 19:08:27 +0100

    Commit for reach the 100 commits, this is the 99 commit.

--- src/main/java/umu/tds/app/PhotoTDS/App.java
+++ src/main/java/umu/tds/app/PhotoTDS/App.java
@@ -4,11 +4,11 @@ import umu.tds.app.PhotoTDS.view.VentanaLogin;
 
 /**
  * 
- * @author quico y JC
+ * @author quico y JC Commit for reach the 100 commits, this is the 99 commit.
  *
  */
 public class App {
   public static void main(String[] args) {
 	  VentanaLogin.main(args);
   }
-}
+} 
